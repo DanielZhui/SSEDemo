94ff3cb5b71069786797ac20e1c61be07f9f9939
wollens <wollens123@163.com>
2023-07-14 17:42:12 +0800

    ⚡️ update posts

--- server/models/post.js
+++ server/models/post.js
@@ -12,6 +12,7 @@ const PostSchema = new Schema(
       default:
         "https://www.google.com.hk/imgres?imgurl=https%3A%2F%2Fclimate.onep.go.th%2Fwp-content%2Fuploads%2F2020%2F01%2Fdefault-image.jpg&tbnid=NfE8TAjA2ODYnM&vet=12ahUKEwiVyp3ulfr_AhVsNkQIHfYQBx8QMygBegUIARDAAQ..i&imgrefurl=https%3A%2F%2Fclimate.onep.go.th%2Fwp-content%2Fuploads%2F2020%2F01%2F&docid=Eh_frbRje_4DLM&w=800&h=600&q=default%20image&ved=2ahUKEwiVyp3ulfr_AhVsNkQIHfYQBx8QMygBegUIARDAAQ",
     },
+    likers: { type: [String], default: [] },
   },
   { timestamps: true }
 );
