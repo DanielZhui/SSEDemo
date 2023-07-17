4ce1847cddc02b5c3c4dc06e1abf67680b5b87af
wollens <wollens123@163.com>
2023-07-17 09:26:10 +0800

    ⚡️ update postCard

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -6,6 +6,8 @@ import {
   MDBCardImage,
   MDBCardSubTitle,
   MDBCardTitle,
+  MDBFooter,
+  MDBIcon,
 } from "mdb-react-ui-kit";
 import { useAppContext } from "../context";
 
@@ -15,13 +17,44 @@ const PostCard = ({ post }) => {
     appDispatch,
   } = useAppContext();
   return (
-    <MDBCard>
+    <MDBCard className="h-100">
       <div>post card</div>
-      <MDBCardImage src={post.photo} />
+      <MDBCardImage
+        src={post.photo}
+        position="top"
+        style={{ maxHeight: 350, objectFit: "cover" }}
+      />
       <MDBCardBody>
         <MDBCardTitle>{post.title}</MDBCardTitle>
-        <MDBCardSubTitle>{post.content}</MDBCardSubTitle>
+        <MDBCardSubTitle style={{ maxWidth: "100%" }}>
+          {post.content}
+        </MDBCardSubTitle>
       </MDBCardBody>
+      <MDBFooter>
+        {post.likers.includes(userId) ? (
+          <>
+            {post.likers.length}
+            <MDBIcon
+              className="likeBtn"
+              size="lg"
+              fas
+              icon="heart"
+              color="danger"
+            />
+          </>
+        ) : (
+          <>
+            {post.likers.length}
+            <MDBIcon
+              className="likeBtn"
+              size="lg"
+              far
+              icon="heart"
+              color="danger"
+            />
+          </>
+        )}
+      </MDBFooter>
       <MDBCardFooter></MDBCardFooter>
     </MDBCard>
   );
